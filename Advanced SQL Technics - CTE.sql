a9555f64181e54c6e24681554a45a29bd90d976c
Akash Saha <sahaakash07@gmail.com>
2026-01-24 12:35:32 +0530

    Update Advanced SQL Technics - CTE.sql

--- Advanced SQL Technics - CTE.sql
+++ Advanced SQL Technics - CTE.sql
@@ -47,7 +47,7 @@ FROM Sales.Orders
 GROUP BY CustomerID
 )
 -- Step3: Rank Customers based on Total Sales Per Customer (Nested CTE)
-, CTE_RANK AS
+, CTE_Customer_RANK AS
 (
 SELECT
 	CustomerID,
@@ -56,12 +56,16 @@ SELECT
 FROM CTE_Total_Sales
 )
 -- Step4: segment customers based on their total sales (Nested CTE)
-, CTE_Customer_Rank AS
+, CTE_Customer_Sgegment AS
 (
 SELECT
 	CustomerID,
 	Total_Sales,
-	RANK() OVER(ORDER BY Total_Sales DESC) AS CustomerRank
+	CASE 
+		WHEN Total_Sales > 100 THEN 'HIGH'
+		WHEN Total_Sales > 50 THEN 'MEDIUM'
+		ELSE 'LOW'
+	END CustomerSegments
 FROM CTE_Total_Sales
 )
 
@@ -71,11 +75,86 @@ SELECT
 	CONCAT(C.FirstName, ' ', C.LastName) AS Full_Name,
 	CTS.Total_Sales,
 	CLO.Last_Order,
-	CCR.CustomerRank
+	CCR.Ranks
 FROM Sales.Customers AS C
 LEFT JOIN CTE_Total_Sales AS CTS		
 ON CTS.CustomerID = C.CustomerID
 LEFT JOIN CTE_Last_Order AS CLO
 ON C.CustomerID = CLO.CustomerID
 LEFT JOIN CTE_Customer_Rank AS CCR
-ON C.CustomerID = CCR.CustomerID;
+ON C.CustomerID = CCR.CustomerID
+LEFT JOIN CTE_Customer_Sgegment AS CCS
+ON C.CustomerID = CCS.CustomerID;
+
+/* ==============================================================================
+   RECURSIVE CTE | GENERATE SEQUENCE
+===============================================================================*/
+
+/* TASK 2:
+   Generate a sequence of numbers from 1 to 20.
+*/
+WITH Series AS (
+    -- Anchor Query
+    SELECT 1 AS MyNumber
+    UNION ALL
+    -- Recursive Query
+    SELECT MyNumber + 1
+    FROM Series
+    WHERE MyNumber < 20
+)
+-- Main Query
+SELECT *
+FROM Series
+
+/* TASK 3:
+   Generate a sequence of numbers from 1 to 1000.
+*/
+WITH Series AS
+(
+    -- Anchor Query
+    SELECT 1 AS MyNumber
+    UNION ALL
+    -- Recursive Query
+    SELECT MyNumber + 1
+    FROM Series
+    WHERE MyNumber < 1000
+)
+-- Main Query
+SELECT *
+FROM Series
+OPTION (MAXRECURSION 5000);
+
+/* ==============================================================================
+   RECURSIVE CTE | BUILD HIERARCHY
+===============================================================================*/
+
+/* TASK 4:
+   Build the employee hierarchy by displaying each employee's level within the organization.
+   - Anchor Query: Select employees with no manager.
+   - Recursive Query: Select subordinates and increment the level.
+ */
+
+WITH CTE_Emp_Hierarchy AS
+(
+    -- Anchor Query: Top-level employees (no manager)
+    SELECT
+        EmployeeID,
+        FirstName,
+        ManagerID,
+        1 AS Level
+    FROM Sales.Employees
+    WHERE ManagerID IS NULL
+    UNION ALL
+    -- Recursive Query: Get subordinate employees and increment level
+    SELECT
+        e.EmployeeID,
+        e.FirstName,
+        e.ManagerID,
+        Level + 1
+    FROM Sales.Employees AS e
+    INNER JOIN CTE_Emp_Hierarchy AS ceh
+        ON e.ManagerID = ceh.EmployeeID
+)
+-- Main Query
+SELECT *
+FROM CTE_Emp_Hierarchy;
