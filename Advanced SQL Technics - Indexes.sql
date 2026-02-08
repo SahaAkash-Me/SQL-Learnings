3f64123f97373dd3bcf4b43b76458cfff316d386
Akash Saha <sahaakash07@gmail.com>
2026-02-08 21:24:05 +0530

    Add files via upload

---
+++ Advanced SQL Technics - Indexes.sql
@@ -0,0 +1,27 @@
+/* ==============================================================================
+   SQL Indexing
+-------------------------------------------------------------------------------
+   This script demonstrates various index types in SQL Server including clustered,
+   non-clustered, columnstore, unique, and filtered indexes. It provides examples 
+   of creating a heap table, applying different index types, and testing their 
+   usage with sample queries.
+
+   Table of Contents:
+	   Index Types:
+			 - Clustered and Non-Clustered Indexes
+			 - Leftmost Prefix Rule Explanation
+			 - Columnstore Indexes
+			 - Unique Indexes
+			 - Filtered Indexes
+		Index Monitoring:
+			 - Monitor Index Usage
+			 - Monitor Missing Indexes
+			 - Monitor Duplicate Indexes
+			 - Update Statistics
+			 - Fragmentations
+=================================================================================
+*/
+
+/* ==============================================================================
+   Structured Indexes : Clustered and Non-Clustered Indexes
+============================================================================== */
